d7aaf8f2ca3e47f28854096bd5e57ee6b935d1fd
Your Name <help.ckrdatapoint@gmail.com>
2026-07-04 13:44:07 +0530

    Fix: Consolidate Material Symbols links to prevent subset override

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=recent_patient" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
